452cff7d9b5fb406d6984589496608ac5db80bae
Jenni Syrjälä <t9syje01@students.oamk.fi>
2020-10-08 13:58:02 +0300

    ex3 ready

--- js/myfunctions.js
+++ js/myfunctions.js
@@ -9,7 +9,20 @@ function loadDoc(page) {
     xhttp.open("GET", page, true);
     xhttp.send();
   }
-  
+  function showLanguage(){
+	  var select=document.getElementById("source_language");
+	  console.log(select);
+	  var source_language=select.value;
+	  console.log(source_language);
+	  var target_language=document.getElementById("target_language");
+	  target_language.value=source_language;
+	}
+
+	function showPhone(phone){
+	  console.log(phone);
+	  var target_phone=document.getElementById("target_phone");
+	  target_phone.value=phone;
+	}
   window.onload=function loadFirst() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
